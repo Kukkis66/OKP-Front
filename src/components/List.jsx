21afdb9e209757448c3621c8ce3ce2bac0538b97
Pimila <128486040+Pimila@users.noreply.github.com>
2024-02-14 13:12:35 +0200

    Commit message describing the changes

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -151,72 +151,8 @@ export const List = ({hubData}) => {
         
 
     </div>
-      
-      <div className='sortContainer'>
-      
-      <div className='dropdown'>
-          <img src={sort} alt="sortLogo" />
-          <a>NÄYTÄ {itemsPerPage}</a>
-          <div className="dropdown-content">
-              <button onClick={() => handleCardCount(6)}>6</button>
-              <button onClick={() => handleCardCount(12)}>12</button>
-              <button onClick={() => handleCardCount(24)}>24</button>
-          </div>
-      </div>
-      
-      {isBackwards ? (
-      <div className='wards'>
-          <span>A - Ö</span>
-          <img onClick={() => handleWards()} src={arrowDown} alt="arrow-down" />
-      </div>
-      ) : (
-      <div className='wards'>
-          <span>Ö - A</span>
-          <img onClick={() => handleWards()} src={arrowDown} alt="arrow-up" style={{ transform: 'rotate(180deg)' }} />
-      </div>
-      )}
-      </div>
 
-      <div className='cardContainer'>
-      <ul >
-          {displayedItems?.map((building) => (
-              <li className="card" key={building.id}>
-              <h2 className='h2'>{getBuildingName(building)}</h2>
-                <div className='info'>
-                      <p className='p'>Osoite: {building.postalAddresses[0]?.streetName}</p>
-                      <p className='p'>Kaupunki: {building.postalAddresses[0]?.city}</p>
-                      <p className='p'>Postinumero: {building.postalAddresses[0]?.postalCode}</p>
-
-                  </div>
-                  <figure className='picture_url'>
-                      <img src={building.productImages[0]?.thumbnailUrl} alt={building.productImages[0]?.altText} />
-                  </figure>
-                  <a className='zoom' onClick={() => handleReadMore(building)}>
-                  LUE LISÄÄ
-                  </a>
-              </li>
-          ))}
-      </ul>
-      {selectedBuilding && <Popup building={selectedBuilding} onClose={() => handleClosePopup()} />}
-      </div>
-      
-      <div className="navigation-arrows">
-      <a onClick={() => handlePageChange(currentPage - 1)}><img src={arrowLeft} alt="arrowLeft" /></a>
-      <div className="pagination">
-          {pageNumbers.map((pageNumber) => (
-              <span
-              key={pageNumber}
-              className={pageNumber === currentPage ? 'active' : ''}
-              onClick={() => handlePageChange(pageNumber)}
-              >
-              {pageNumber}
-              </span>
-          ))}
-      </div>
-          <a onClick={() => handlePageChange(currentPage + 1)}><img src={arrowRight} alt="arrowRight" /></a>
-      </div>
-      
-    </div>
-    );
+);
 }
+
 
