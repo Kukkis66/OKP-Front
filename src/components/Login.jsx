34f20aa07027bce5a8861c8492b3925105de136a
Roman <91057166+SweetCinnamonBun@users.noreply.github.com>
2024-02-22 02:24:55 +0200

    made a login function

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,7 +6,7 @@ import '../styles/Login.css'
 
 export const Login = ({loginForm, handleLoginForm}) => {
     const [newUser, setNewUser] = useState(false)
-    const { login } = useAuth();
+    const { login, loginUser } = useAuth();
 
 
 
@@ -61,31 +61,37 @@ export const Login = ({loginForm, handleLoginForm}) => {
                 <div className='loginContainer'>
     
                 <h2 className='loginHeader'>KIRJAUDU SISÄÄN</h2>
-                <div className='LoginInputFieldsContainer'>
-                  <div className='inputs inputsLogin'>
-                    <p>Käyttäjätunnus:</p>
-                    <input
-                    className="inputField inputFieldLogin"
-                    type="text"
-                    placeholder="nimi"
-                    />
+                <form onSubmit={loginUser}>
+
+                  <div className='LoginInputFieldsContainer'>
+                    <div className='inputs inputsLogin'>
+                      <p>Käyttäjätunnus:</p>
+                      <input
+                      className="inputField inputFieldLogin"
+                      type="text"
+                      placeholder="nimi"
+                      name="email"
+                      />
+                    </div>
+                    <div className='inputs inputsLogin'>
+                      <p>Salasana:</p>
+                      <input
+                      className="inputField inputFieldLogin"
+                      type="text"
+                      placeholder="salasana"
+                      name="password"
+                      />
+                    </div>
                   </div>
-                  <div className='inputs inputsLogin'>
-                    <p>Salasana:</p>
-                    <input
-                    className="inputField inputFieldLogin"
-                    type="text"
-                    placeholder="salasana"
-                    />
+                  <div className='loginCenter'>
+                    <button className='button buttonLogin' onClick={() => { login(); handleLoginForm(); }}>KIRJAUDU SISÄÄN</button>
                   </div>
-                </div>
-                <div className='loginCenter'>
-                  <button className='button buttonLogin' onClick={() => { login(); handleLoginForm(); }}>KIRJAUDU SISÄÄN</button>
-                </div>
-                <div className='newUser'>
-                  <span>Oletko uusi käyttäjä?</span>
-                  <a href='#' onClick={handleNewUser}>LUO KÄYTTÄJÄTILI</a>
-                </div>
+                  <div className='newUser'>
+                    <span>Oletko uusi käyttäjä?</span>
+                    <a href='#' onClick={handleNewUser}>LUO KÄYTTÄJÄTILI</a>
+                  </div>
+                  <input type="submit"/>
+                </form>
                 </div>)}
               </div>
             </div>
--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,10 +1,12 @@
 // AuthContext.js
 import { createContext, useContext, useState } from 'react';
+import axios from 'axios';
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [isLoggedIn, setLoggedIn] = useState(false);
+  const [authToken, setAuthToken] = useState(null);
 
   const login = () => {
     // Implement your login logic here
@@ -16,8 +18,41 @@ export const AuthProvider = ({ children }) => {
     setLoggedIn(false);
   };
 
+  const loginUser = async (e) => {
+    e.preventDefault();
+    console.log('Form submitted');
+    let response = await fetch('http://localhost:5143/api/Account/login', {
+      method:'POST',
+      headers: {
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify({'email':e.target.email.value, 'password':e.target.password.value})
+    });
+    if (response.status === 200)
+    {
+      let data = await response.json()
+      console.log(data);
+      console.log(data.jwtToken);
+    } else {
+      let errormessage = await response.text();
+      console.log(errormessage);
+    }
+    
+  };
+
+  // const loginUser = (e) => {
+  //   let loginObject = {"email": e.target.email.value, "password": e.target.password.value};
+
+  //   let data = axios.post('http://localhost:5143/api/Account/login', loginObject).then(response => response.data);
+  //   console.log(data);
+  // };
+
+  // let contextData = {
+  //   loginUser:loginUser
+  // };
+
   return (
-    <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+    <AuthContext.Provider value={{ isLoggedIn, login, logout, loginUser}}>
       {children}
     </AuthContext.Provider>
   );
